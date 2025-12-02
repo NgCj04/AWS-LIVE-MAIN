90e8acb8b661c596bc4a0d7341b0a64751959f06
NgCj04 <ngcj-wp22@student.tarc.edu.my>
2025-12-02 11:24:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnsmkcww69uq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ngbucket0531"
+customregion = "us-east-1"
+
