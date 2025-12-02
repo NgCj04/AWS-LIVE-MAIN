623d9757894ae66edebf5aa1709249a7fcde8e5d
NgCj04 <ngcj-wp22@student.tarc.edu.my>
2025-12-02 11:32:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cnsmkcww69uq.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "bait3273"
 customdb = "employee"
 custombucket = "ngbucket0531"
 customregion = "us-east-1"
